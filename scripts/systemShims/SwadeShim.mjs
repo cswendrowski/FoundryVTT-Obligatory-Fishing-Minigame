fc55a42fc7bd4a0c4fcd78a70f05c68c14272d6b
FloRad <florad92@gmail.com>
2022-09-14 19:12:57 +0200

    added support for savage worlds

---
+++ scripts/systemShims/SwadeShim.mjs
@@ -0,0 +1,41 @@
+export class SwadeShim {
+  constructor(character) {
+    this.character = character;
+  }
+
+  // Catch faster
+  get str() {
+    const die = this.character.system.abilities.strength.die.sides;
+    return (die / 2) - 2;
+  }
+
+  // Bait moves faster
+  get con() {
+    const die = this.character.system.abilities.vigor.die.sides;
+    return (die / 2) - 2;
+  }
+
+  // Increased chance of treasure
+  get int() {
+    if (this.character.type === 'vehicle') return 0;
+    this.character.system.bennies.max;
+  }
+
+  // Speed of fish is reduced
+  get wis() {
+    const die = this.character.system.abilities.spirit.die.sides;
+    return (die / 2) - 2;
+  }
+
+  // How far the fish jumps is reduced
+  get cha() {
+    const die = this.character.system.abilities.spirit.die.sides;
+    return (die / 2) - 2;
+  }
+
+  // The fish moves less often
+  get dex() {
+    const die = this.character.system.abilities.agility.die.sides;
+    return (die / 2) - 2;
+  }
+}
