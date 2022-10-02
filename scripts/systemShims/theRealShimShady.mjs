5e46415fab8147d827238cad2ef307b28bb42fe2
EndlesNights <endlesnights@gmail.com>
2022-10-02 08:30:17 -0400

    dnd4e system support

--- scripts/systemShims/theRealShimShady.mjs
+++ scripts/systemShims/theRealShimShady.mjs
@@ -1,3 +1,4 @@
+import {Dnd4eShim} from "./Dnd4eShim.mjs";
 import {Dnd5eShim} from "./Dnd5eShim.mjs";
 import {Pf2eShim} from "./Pf2eShim.mjs";
 import {ThirteenthAgeShim} from "./13a.mjs";
@@ -6,6 +7,7 @@ import { SwadeShim } from "./SwadeShim.mjs";
 export function systemShim(character) {
   const system = game.system.id;
   switch ( system ) {
+    case "dnd4e": return new Dnd4eShim(character);
     case "dnd5e": return new Dnd5eShim(character);
     case "pf2e": return new Pf2eShim(character);
     case "archmage": return new ThirteenthAgeShim(character);
